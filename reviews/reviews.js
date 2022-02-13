46ea946451473b392cc4404d3dbd8870b6b778ea
nickabramowicz@gmail.com <nickabramowicz@gmail.com>
2022-02-13 12:10:38 -0800

    Reviews practice JS complete

--- reviews/reviews.js
+++ reviews/reviews.js
@@ -37,59 +37,111 @@ const reviews = [
     },
   ];
 
-// select items
-const img = document.getElementById("person-img");
-const author = document.getElementById("author");
-const job = document.getElementById("job");
-const info = document.getElementById("info");
 
-const prevBtn = document.querySelector(".prev-btn");
-const nextBtn = document.querySelector(".next-btn");
-const randomBtn = document.querySelector(".random-btn");
+const img = document.getElementById('person-img')
+const author = document.getElementById('author')
+const job = document.getElementById('job')
+const info = document.getElementById('info')
 
-// set starting item
+const prev = document.querySelector('.prev-btn')
+const next = document.querySelector('.next-btn')
+const random = document.querySelector('.random-btn')
+
 let currentItem = 0;
 
-// load initial item
-window.addEventListener("DOMContentLoaded", function () {
-  const item = reviews[currentItem];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
-});
+window.addEventListener('DOMContentLoaded', () => {
+    showPerson();
+})
 
-// show person based on item
-function showPerson(person) {
-  const item = reviews[person];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
+const showPerson = () => {
+    let current = reviews[currentItem];
+    img.src = current.img;
+    author.textContent = current.name;
+    job.textContent = current.job;
+    info.textContent = current.text;
 }
 
-// show next person
-nextBtn.addEventListener("click", function () {
-  currentItem++;
-  if (currentItem > reviews.length - 1) {
-    currentItem = 0;
-  }
-  showPerson(currentItem);
-});
+next.addEventListener('click', () => {
+    currentItem++;
+    
+    if(currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
 
-// show prev person
-prevBtn.addEventListener("click", function () {
-  currentItem--;
-  if (currentItem < 0) {
-    currentItem = reviews.length - 1;
-  }
-  showPerson(currentItem);
-});
+    showPerson();
+})
 
-// show random person
-randomBtn.addEventListener("click", function () {
-  console.log("hello");
+prev.addEventListener('click', () => {
+    currentItem--;
+    
+    if(currentItem < 0) {
+        currentItem = reviews.length - 1;
+    }
 
-  currentItem = Math.floor(Math.random() * reviews.length);
-  showPerson(currentItem);
-});
+    showPerson();
+})
+
+random.addEventListener('click', () => {
+    currentItem = Math.floor(Math.random() * reviews.length)
+
+    showPerson();
+})
+
+
+// // select items
+// const img = document.getElementById("person-img");
+// const author = document.getElementById("author");
+// const job = document.getElementById("job");
+// const info = document.getElementById("info");
+
+// const prevBtn = document.querySelector(".prev-btn");
+// const nextBtn = document.querySelector(".next-btn");
+// const randomBtn = document.querySelector(".random-btn");
+
+// // set starting item
+// let currentItem = 0;
+
+// // load initial item
+// window.addEventListener("DOMContentLoaded", function () {
+// //   const item = reviews[currentItem];
+// //   img.src = item.img;
+// //   author.textContent = item.name;
+// //   job.textContent = item.job;
+// //   info.textContent = item.text;
+//     showPerson(currentItem);
+// });
+
+// // show person based on item
+// function showPerson(person) {
+//   const item = reviews[person];
+//   img.src = item.img;
+//   author.textContent = item.name;
+//   job.textContent = item.job;
+//   info.textContent = item.text;
+// }
+
+// // show next person
+// nextBtn.addEventListener("click", function () {
+//   currentItem++;
+//   if (currentItem > reviews.length - 1) {
+//     currentItem = 0;
+//   }
+//   showPerson(currentItem);
+// });
+
+// // show prev person
+// prevBtn.addEventListener("click", function () {
+//   currentItem--;
+//   if (currentItem < 0) {
+//     currentItem = reviews.length - 1;
+//   }
+//   showPerson(currentItem);
+// });
+
+// // show random person
+// randomBtn.addEventListener("click", function () {
+//   console.log("hello");
+
+//   currentItem = Math.floor(Math.random() * reviews.length);
+//   showPerson(currentItem);
+// });
